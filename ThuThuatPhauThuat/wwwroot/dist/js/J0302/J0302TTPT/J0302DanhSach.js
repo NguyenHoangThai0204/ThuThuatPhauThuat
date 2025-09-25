66f612d8fbc6d48d8cdff5ccf6e48ddbc01c2bdf
NguyenHoangThai0204 <thaidp2002@gmail.com>
2025-09-25 16:15:46 +0700

    done

---
+++ ThuThuatPhauThuat/wwwroot/dist/js/J0302/J0302TTPT/J0302DanhSach.js
@@ -0,0 +1,43 @@
+﻿function formatDateTime(date) {
+    const dd = String(date.getDate()).padStart(2, '0');
+    const MM = String(date.getMonth() + 1).padStart(2, '0');
+    const yyyy = date.getFullYear();
+    const HH = String(date.getHours()).padStart(2, '0');
+    const mm = String(date.getMinutes()).padStart(2, '0');
+    return `${dd}-${MM}-${yyyy} ${HH}:${mm}`;
+}
+
+function updateDateTime() {
+    var now = new Date();
+    var formatted = formatDateTime(now);
+    $("#info-datetime", window.parent.document).text(formatted);
+}
+
+$(document).on("click", "#example tbody tr", function () {
+    var tenBN = $(this).find("td:eq(2)").text().trim();
+    var namSinh = $(this).find("td:eq(3)").text().trim();
+    var gioiTinh = $(this).find("td:eq(4)").text().trim();
+
+    var bacSi = $(this).find("td:eq(10)").text().trim();
+
+    $("#info-tenbn", window.parent.document).text(tenBN);
+    $("#info-namsinh", window.parent.document).text(namSinh + " - " + gioiTinh);
+    $("#info-bacsi", window.parent.document).text(bacSi);
+    updateDateTime();
+
+    $("#example tbody tr").removeClass("table-active");
+    $(this).addClass("table-active");
+});
+
+$(document).ready(function () {
+    updateDateTime();
+    setInterval(updateDateTime, 60000);
+});
+$(document).ready(function () {
+    $('#example').DataTable({
+        scrollX: true,
+        paging: false,
+        searching: false,
+        info: false
+    });
+});
---
+++ ThuThuatPhauThuat/wwwroot/dist/js/J0302/J0302TTPT/J0302Index.js
@@ -0,0 +1,15 @@
+﻿$(document).ready(function () {
+    // load Danh sách mặc định
+    $("#tabs-danhsach-7").load("/thu_thuat_phau_thuat/danh_sach");
+
+    // khi click tab thì load nội dung
+    $('a[data-bs-toggle="tab"]').on('shown.bs.tab', function (e) {
+        var target = $(e.target).attr("href");
+        if (target === "#tabs-danhsach-7" && $(target).is(':empty')) {
+            $(target).load("/thu_thuat_phau_thuat/danh_sach");
+        }
+        else if (target === "#tabs-thongtin-7" && $(target).is(':empty')) {
+            $(target).load("/thu_thuat_phau_thuat/thong_tin");
+        }
+    });
+});
